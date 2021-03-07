ee3f4b84dc7941f10b236130d9310f7e97b88c87
yasin javar <yasinjjjv@gmail.com>
2021-03-07 13:22:23 +0330

    add Guide in bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,12 @@ bot = Client(
     api_hash= "sdfgbdrseaeerte4345432345yt", # str onf API HASH from my.telegram.org
     # config_file="config.ini"
 )
-
+CHANNEL_ID = 'telegram' #without ' @ ' or ' t.me/ '
 
 @bot.on_message(filters.private)
 def all_message(client, m):
     USER_CHAT_ID = m.from_user.id
+    
     try:
         with open("sended_mdg_id.txt", 'r') as f:  # read database
             for ids in f:
@@ -19,8 +20,9 @@ def all_message(client, m):
                     return
     except:
         print('Something went wrong !!')
+    
     i = 0
-    for member in client.iter_chat_members('testingssq'):
+    for member in client.iter_chat_members(CHANNEL_ID):
         if int(USER_CHAT_ID) == int(member.user.id):
             i += 1
             break
@@ -29,8 +31,9 @@ def all_message(client, m):
         pass
     else:
         m.delete()
+        # Here you can edit th message
         m.reply_text(
-            "Pv locked to this chat(channel/group) @testingssq\nJoin And Send messsage again :)")
+            f"Pv locked to this chat(channel/group)\n@{CHANNEL_ID}\nJoin And Send messsage again :)")
         # Add id to Database for don't repat join waring :)
         with open("sended_mdg_id.txt", 'w') as f:
             f.write(f"{USER_CHAT_ID}\n")
